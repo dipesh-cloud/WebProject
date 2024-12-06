e3bc578ddde2e0a81cdc5d6f6d480eb4dd113865
dipesh-cloud <dshah-baniya@myseneca.ca>
2024-12-06 18:45:04 -0500

    Updated project with new features and views for auth-service, login, register, and user history

--- modules/auth-service.js
+++ modules/auth-service.js
@@ -15,7 +15,17 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
-mongoose.connect(process.env.MONGODB, {
+// Log the MongoDB URI for debugging
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+// Check if MONGO_URI is loaded
+if (!process.env.MONGO_URI) {
+  console.error('Error: MONGO_URI is not defined in the environment variables.');
+  process.exit(1); // Exit the app
+}
+
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
